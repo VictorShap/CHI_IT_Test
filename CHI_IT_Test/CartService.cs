aab58f44317b012295a1937d4298483becfdc824
Yana <yanadavidenko999@gmail.com>
2025-05-26 20:47:43 +0200

    just added cart service

---
+++ CHI_IT_Test/CartService.cs
@@ -0,0 +1,14 @@
+﻿namespace CHI_IT_Test
+{
+    internal class CartService
+    {
+        private readonly Cart _cart;
+
+        public CartService(Cart cart)
+        {
+            _cart = cart;
+        }
+
+
+    }
+}
--- CHI_IT_Test/Program.cs
+++ CHI_IT_Test/Program.cs
@@ -3,7 +3,7 @@
     class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             List<Product> products = CreateProducts();
 
             DisplayProducts(products);
@@ -29,6 +29,38 @@
             foreach (Product product in products)
             {
                 Console.WriteLine("Name: " + product.Name + " " + "Price: " + product.Price);
+            }
+        }
+
+        public static void SelectProducts()
+        {
+            Console.WriteLine("Select the product by wrtiting its name. Type \"0\" in order to proceed to enter your budget");
+
+            string product = Console.ReadLine();
+
+            while (product != "0")
+            {
+                switch (product)
+                {
+                    case "Laptop":
+
+                        break;
+                    case "Smartphone":
+
+                        break;
+                    case "Headphones":
+
+                        break;
+                    case "Speakers":
+
+                        break;
+                    case "Keyboard":
+
+                        break;
+                    default:
+
+                        break;
+                }
             }
         }
     }
