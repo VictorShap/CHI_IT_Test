47429ff139fdd3ca412c4f4ee8e3b6d714be5db6
Yana <yanadavidenko999@gmail.com>
2025-05-26 13:53:32 +0200

    Add project files.

---
+++ CHI_IT_Test/Cart.cs
@@ -0,0 +1,17 @@
+﻿namespace CHI_IT_Test
+{
+    internal class Cart
+    {
+        private List<Product> products;
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public decimal GetTotalAmount()
+        {
+            return 0;
+        }
+    }
+}
---
+++ CHI_IT_Test/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CHI_IT_Test
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Product> products = CreateProducts();
+
+            DisplayProducts(products);
+        }
+
+        public static List<Product> CreateProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("Laptop", 999.99m));
+            products.Add(new Product("Smartphone", 499.50m));
+            products.Add(new Product("Headphones", 149.95m));
+            products.Add(new Product("Speakers", 200.95m));
+            products.Add(new Product("Keyboard", 100.99m));
+
+            return products;
+        }
+
+        public static void DisplayProducts(List<Product> products)
+        {
+            Console.WriteLine("List of available products:");
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine("Name: " + product.Name + " " + "Price: " + product.Price);
+            }
+        }
+    }
+}
